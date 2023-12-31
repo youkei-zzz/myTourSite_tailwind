e53354cca0884d7c5c6a3dad45900665b4b630da
will <1826340163@qq.com>
2023-12-31 18:00:01 +0800

    测试链接部署

--- src/components/interface/searchItem.ts
+++ src/components/interface/searchItem.ts
@@ -1,12 +1,14 @@
-export default interface searchItems {
-	address: string;
-	id: number;
-	level: string;
-	location: string;
-	name: string;
-	price: number;
-	province: string;
-	rating: number;
-	sales: number;
-	slogan: string;
+type searchItems = {
+	address: string
+	id: number
+	level: string
+	location: string
+	name: string
+	price: number
+	province: string
+	rating: number
+	sales: number
+	slogan: string
 }
+
+export default searchItems
